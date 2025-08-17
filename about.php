c949dfc681c61058e61af476d696e09d36a1b637
nihafarheen758 <nihafarheen758@gmail.com>
2025-08-17 15:16:43 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Boat Booking System || About</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Dancing+Script:400,700|Muli:300,400" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+  <link rel="stylesheet" href="css/aos.css">
+  <link href="css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
+
+  <link rel="stylesheet" href="css/style.css">
+
+
+
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+  <div class="site-wrap">
+<?php include_once("includes/navbar.php");?>
+    
+    
+
+      <div class="intro-section" style="background-image: url('images/kerala1.jpg');">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-7 mx-auto text-center" data-aos="fade-up">
+              <h1>About WaterBoat</h1>
+        
+              <p><a href="contact.php" class="btn btn-primary py-3 px-5">Contact</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="images/kerala1.jpg" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-md-6">
+            <span class="text-serif text-primary">About Us</span>
+            <h3 class="heading-92913 text-black">Welcome To Our Website</h3>
+            <p>Imagine yourself sailing through crystal-clear waters, surrounded by breathtaking scenery and the warm sun on your skin. At BOATIFY, we make it possible for you to experience the thrill of boating, whether you're looking for a relaxing getaway, an adrenaline-pumping adventure, or a special celebration with friends and family.As a leading provider of boat rentals and charters, we've curated a diverse fleet of vessels to suit every need and preference. From luxurious yachts to speedboats, pontoon boats, and fishing boats, our collection has something for everyone.With BOATIFY, you're not just renting a boat – you're unlocking a world of possibilities. Our expert team is dedicated to providing personalized service, expert guidance, and unparalleled support to ensure that your boating experience is nothing short of exceptional.Whether you're a seasoned boater or a newcomer to the world of boating, we invite you to join us on the water and discover the magic of BOATIFY.
+</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+  <?php include_once("includes/footer.php");?>
+    
+
+  </div>
+  <!-- .site-wrap -->
+
+
+  <!-- loader -->
+  <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#ff5e15"/></svg></div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.mb.YTPlayer.min.js"></script>
+
+
+
+
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
